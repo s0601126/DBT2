77780f1869530f6162fdaff9eb7cbdd4f4b67bfa
s0601126 <161266658+s0601126@users.noreply.github.com>
2024-04-22 08:17:55 +0800

    Add files via upload

---
+++ DBTutorial/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace DBTutorial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstBox1 = new System.Windows.Forms.ListBox();
+            this.lstBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstBox1
+            // 
+            this.lstBox1.FormattingEnabled = true;
+            this.lstBox1.ItemHeight = 12;
+            this.lstBox1.Location = new System.Drawing.Point(3, 34);
+            this.lstBox1.Name = "lstBox1";
+            this.lstBox1.Size = new System.Drawing.Size(120, 388);
+            this.lstBox1.TabIndex = 0;
+            // 
+            // lstBox2
+            // 
+            this.lstBox2.FormattingEnabled = true;
+            this.lstBox2.ItemHeight = 12;
+            this.lstBox2.Location = new System.Drawing.Point(145, 34);
+            this.lstBox2.Name = "lstBox2";
+            this.lstBox2.Size = new System.Drawing.Size(120, 388);
+            this.lstBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Employee number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(143, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "First Name + Last Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 588);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstBox2);
+            this.Controls.Add(this.lstBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstBox1;
+        private System.Windows.Forms.ListBox lstBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ DBTutorial/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBTutorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (var classicContext = new classicmodelsEntities())
+            {
+                var allEmpNum = from list in classicContext.employees
+                                select list.employeeNumber; //select employeeNumber from employees
+                                                            //var emp = classContext.employees.Select(i => i.employeeNumber);
+                foreach (var empno in allEmpNum.ToList())
+                {
+                    lstBox1.Items.Add(empno);
+                }
+
+                //new codes – filtering by Email and Employee Number
+                var allEmpNum1 = classicContext.employees.Where(i => i.email.Contains("ab") || i.employeeNumber == 1501).Select(i => i.employeeNumber);
+
+
+                //Existing codes – No filtering, get ALL employee numbers
+                var allEmpNum2 = classicContext.employees.Select(i => i.employeeNumber);
+
+                var allFirstLastName = from list in classicContext.employees
+                                       select new { list.firstName, list.lastName };
+                //select firstName as a, LastName as b from employees
+                foreach (var empno in allFirstLastName.ToList())
+                {
+                    lstBox2.Items.Add(empno.firstName + " " + empno.lastName);
+                }
+
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
